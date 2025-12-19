4e383d13b47fbf629a65c7b59560847d6f535434
antoniorodriguezlepe <antonio.rodriguezlepe@gmail.com>
2025-12-19 11:29:48 +0100

    Comments

--- Dia1/dia1-1.py
+++ Dia1/dia1-1.py
@@ -3,14 +3,16 @@ direccion = "0"
 total = 0
 valor = 0
 dial = 50
+# Open file and read by lines
 with open(file_path, 'r') as file:
     for line in file:
+        # parse imput into address (L or R) and displacement value 
         direccion = line[0]
         valor = int(line[1:].strip())
-        if direccion == "R":
+        if direccion == "R": # move right
             dial = (dial + valor) % 100
-        elif direccion == "L":
+        elif direccion == "L": # move left
             dial = (dial - valor) % 100
-        if dial == 0:
+        if dial == 0: # Check if final position is at 0
             total += 1
-    print(total)
+    print(total) # Result
--- Dia1/dia1-2.py
+++ Dia1/dia1-2.py
@@ -3,15 +3,17 @@ direccion = "0"
 total = 0
 valor = 0
 dial = 50
+# Open file and read by lines
 with open(file_path, 'r') as file:
     for line in file:
+        # parse imput into address (L or R) and displacement value
         direccion = line[0]
         valor = int(line[1:].strip())
         if direccion == "R":
-            total += (dial + valor) // 100
-            dial = (dial + valor) % 100
+            total += (dial + valor) // 100 # Check how many times we pass 0 moving right
+            dial = (dial + valor) % 100 # Move right
         elif direccion == "L":
-            distancia_ajustada = (100 - dial) % 100
-            total += (valor + distancia_ajustada) // 100
-            dial = (dial - valor) % 100
-    print(total)
+            distancia_ajustada = (100 - dial) % 100 # Calculate how far we are from 0
+            total += (valor + distancia_ajustada) // 100 # Check how many times we pass 0 moving left
+            dial = (dial - valor) % 100 # Move left
+    print(total) # Result
